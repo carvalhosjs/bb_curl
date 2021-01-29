dcb2a11f83c1588d13cbf327a7c623365f20d15f
medtradev <informatica@medtra.com.br>
2021-01-29 07:43:32 -0300

    ok

--- index.php
+++ index.php
@@ -11,16 +11,10 @@ $url = "https://api.dropboxapi.com/2/files/list_folder";
 //$url = "https://content.dropboxapi.com/2/files/download";
 
 
-//$a = (new Request($url))
-//    ->setHeader("Authorization", "Bearer x3iy2r0NEsoAAAAAAAAAAa3ZTNXYwU_VwkIAc7yuGB9jeI9fNHEF06h-IhpJoSCw")
-//    ->setHeader("Dropbox-API-Select-User", "dbmid:AAA9rjSU7Gg1W99Yvg7DUF43I0D9AkYv1vA")
-//    ->setJsonHeader('Dropbox-API-Arg', ['path' => 'id:X2mWIH6F-6AAAAAAACByEQ'])
-//    ->post()->download("teste.pdf")->withErrors();
-
 $a = (new Request($url))
     ->withJson()
-    ->setHeader("Authorization", "Bearer x3iy2r0NEsoAAAAAAAAAAa3ZTNXYwU_VwkIAc7yuGB9jeI9fNHEF06h-IhpJoSCw")
-    ->setHeader("Dropbox-API-Select-User", "dbmid:AAA9rjSU7Gg1W99Yvg7DUF43I0D9AkYv1vA")
+    ->setHeader("Authorization", "")
+    ->setHeader("Dropbox-API-Select-User", "")
     ->post(["path" => "/digital/pdf/documentos_assinados/0-671"])->run()->data();
 
 var_dump($a);
